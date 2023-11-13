0473672aaea29ce471193fb6419924c2b0654733
w1zz4rddr4g0n <wizarddragonpublic@gmail.com>
2023-11-13 14:14:38 -0700

    i don't understand my own configs anymore but unstable seems to work D:

--- Modules/unstable-gui.nix
+++ Modules/unstable-gui.nix
@@ -1,8 +1,11 @@
-{ nixpkgs-unstable, ... }:
+{ nixpkgs-unstable, pkgs, ... }:
 {
-    environment.systemPackages = with nixpkgs-unstable; [
-        prusa-slicer
+# I hate everything going on here but it seems to work so it must stay :(
+    nixpkgs.overlays = [
+        (final: prev: {
+            inherit (nixpkgs-unstable.legacyPackages.${pkgs.system}) prusa-slicer;
+            inherit (nixpkgs-unstable.legacyPackages.${pkgs.system}) blender;
+        })
         # this package of blender is not cuda compatible need to enable cuda support and use "blender-bin" for optix denoising to work but thats a problem for later.
-        blender
     ];
 }
