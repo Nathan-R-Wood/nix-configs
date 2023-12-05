1047b5eb6cbbf82f6e031d7c865ac3de41792d4c
wizard dragon <wizarddragonpublic@gmail.com>
2023-12-04 23:02:06 -0700

    added devin im literally god he didnt exist before now

---
+++ Modules/defin.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }: {
+
+users.users.defin = {
+    isNormalUser = true;
+    description = "Dumbest nerd alive";
+    extraGroups = [];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrDUymBW+rosSI68sxDKLMfH7cOPPLG5K+CuA6aYcsp defin@nixos"
+    ];
+  };
+nix.settings.allowed-users = ["defin" "allthebeans"];
+}
