c57c5c3aa93543e41a921b9a06cd3abf420687c3
wizard dragon <wizarddragonpublic@gmail.com>
2023-12-04 23:19:46 -0700

    builders group

--- Modules/defin.nix
+++ Modules/defin.nix
@@ -3,10 +3,10 @@
 users.users.defin = {
     isNormalUser = true;
     description = "Dumbest nerd alive";
-    extraGroups = [];
+    extraGroups = ["builders"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrDUymBW+rosSI68sxDKLMfH7cOPPLG5K+CuA6aYcsp defin@nixos"
     ];
   };
-nix.settings.allowed-users = ["defin" "allthebeans"];
+nix.settings.allowed-users = ["@builders" "@wheel"];
 }
