560a335890e97686537974300ed2f908984bdf99
wizard dragon <wizarddragonpublic@gmail.com>
2023-12-05 01:24:16 -0700

    helix

--- Modules/defin.nix
+++ Modules/defin.nix
@@ -4,6 +4,7 @@ users.users.defin = {
     isNormalUser = true;
     description = "Dumbest nerd alive";
     extraGroups = ["builders"];
+    packages = with pkgs; [helix];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrDUymBW+rosSI68sxDKLMfH7cOPPLG5K+CuA6aYcsp defin@nixos"
     ];
