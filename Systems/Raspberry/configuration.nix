3c002266a0e7e9a431041eb6b52ec5c3a55d9474
Nathan-R-Wood <nathanroywood@proton.me>
2024-05-16 15:59:45 -0600

    more raspi

--- Systems/Raspberry/configuration.nix
+++ Systems/Raspberry/configuration.nix
@@ -9,13 +9,15 @@ imports =
       ./Blaidd.nix
     ];
 
-  # Newer Kernel
-
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+boot = {
+  kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+  initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+  loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
+};
 
   networking.hostName = "Blaidd"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -44,38 +46,6 @@ imports =
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = false;
-  
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = false;
-  services.xserver.desktopManager.plasma5.enable = false;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = false;
