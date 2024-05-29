dca02dfbccfff83b725845f1815a31864c60588e
Nathan-R-Wood <nathanroywood@proton.me>
2024-05-28 21:26:04 -0600

    spelling again

---
+++ Modules/vpn-client.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }: {
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
+  };
+  networking.wireguard.interfaces = {
+    wg0 = {
+        ips = ["10.69.69.2/24"];
+        privateKeyFile = "/home/allthebeans/wireguard-keys/private";
+        peers = [
+            {
+                publicKey = "MMaorcJeSQBLpudkek5owM4FXY1Bnfa5N1f2tEZuG3s=";
+                allowedIPs = ["10.69.69.2"];
+                endpoint = "192.168.0.25:51820";
+                persistentKeepalive = 25;
+            }
+        ];
+    };
+  };
+}
