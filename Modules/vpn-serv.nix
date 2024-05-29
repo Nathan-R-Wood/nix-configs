bc7b50c4dd947056a2666d2235e33f951bbdfd57
Nathan-R-Wood <nathanroywood@proton.me>
2024-05-29 02:53:33 -0600

    removed endpoint from server side wireguard

--- Modules/vpn-serv.nix
+++ Modules/vpn-serv.nix
@@ -1,8 +1,4 @@
 { config, pkgs, ... }: {
-
-  #networking.nat.enable = true;
-  #networking.nat.externalInterface = "wlan0";
-  #networking.nat.internalInterfaces = ["wg0"];
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
   };
@@ -16,7 +12,6 @@
               {
                   publicKey = "Cy3POfXjG0iJzfNEkirAOrt8TVldEo8ez5pT2KVBXyA=";
                   allowedIPs = ["10.69.69.0/24"];
-                  endpoint = "192.168.0.134:51820";
               }
             ];
       };
