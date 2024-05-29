769bc14e1d22dc2808e7a05782c21eadca5088b2
Nathan-R-Wood <nathanroywood@proton.me>
2024-05-28 21:02:24 -0600

    wiregaurd server

--- Modules/vpn-serv.nix
+++ Modules/vpn-serv.nix
@@ -11,7 +11,17 @@
     wg0 = {
       ips = [10.69.69.1/24];
       listenport = 51820
+      privateKeyFile = "/home/allthebeans/wiregaurd-keys/private"
+        peers = [
+          {
+            # Blaidd
+            publicKey = "MMaorcJeSQBLpudkek5owM4FXY1Bnfa5N1f2tEZuG3s=";
+            allowedIPs = ["10.69.69.2/32"];
+          }
+          {
+            # TBD
+          }
+        ]
     };
   };
-
 }
