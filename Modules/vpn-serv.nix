1eda7897796faee75a821ee7f644f25cb54bb768
Nathan-R-Wood <nathanroywood@proton.me>
2024-05-29 00:36:16 -0600

    wireguard testing

--- Modules/vpn-serv.nix
+++ Modules/vpn-serv.nix
@@ -16,6 +16,7 @@
               {
                   publicKey = "mP28sB+ykEQr6ajtvYEs9WQHpZL08tK2Hw0tS3AX52c=";
                   allowedIPs = ["10.69.69.0/24"];
+                  endpoint = "192.168.0.134";
               }
             ];
       };
