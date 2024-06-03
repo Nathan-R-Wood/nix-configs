e655d3bd7162f788d4bdbb6ea6ff0d9d90c5fe30
Nathan-R-Wood <nathanroywood@proton.me>
2024-06-02 19:00:57 -0600

    fwupd is enbled doesn't also need to be downloaded as a package

--- Modules/utilities.nix
+++ Modules/utilities.nix
@@ -5,20 +5,19 @@
         usbutils
         btop
         git
-        fwupd
     ];
     # Lets some things get firmware updates which probably only helps.
-    services.fwupd.enable = true;
+    services.fe = true;
     #fixes some program compatibiility and doesn't seem to hurt anything.
     boot.kernel.sysctl."vm.max_map_count" = 2147483642;
 
-    services.openssh = {
+.openssh = {
       enable = true;
       settings.PasswordAuthentication = false;
-      settings.PermitRootLogin = "no";
+s.PermitRootLogin = "no";
       settings.KbdInteractiveAuthentication = false;
       # Changed from the default port of 22 so gitlab can use 22
-      ports = [24];
+ [24];
     };
 
 }
