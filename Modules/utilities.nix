71f40cd680f89185affd5d9b40b8ce9cb9eb5b69
Nathan-R-Wood <nathanroywood@proton.me>
2024-06-02 19:02:04 -0600

    fwupd is enbled doesn't also need to be downloaded as a package

--- Modules/utilities.nix
+++ Modules/utilities.nix
@@ -7,17 +7,17 @@
         git
     ];
     # Lets some things get firmware updates which probably only helps.
-    services.fe = true;
+    services.fwupd.enable = true;
     #fixes some program compatibiility and doesn't seem to hurt anything.
     boot.kernel.sysctl."vm.max_map_count" = 2147483642;
 
-.openssh = {
+    services.openssh = {
       enable = true;
       settings.PasswordAuthentication = false;
-s.PermitRootLogin = "no";
+      settings.PermitRootLogin = "no";
       settings.KbdInteractiveAuthentication = false;
       # Changed from the default port of 22 so gitlab can use 22
- [24];
+      ports = [24];
     };
 
 }
