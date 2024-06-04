e332da31b8764ff2cb663b71f9ab6b02c47612f9
Nathan-R-Wood <nathanroywood@proton.me>
2024-06-04 11:11:54 -0600

    testing path

--- Modules/Gui/home.nix
+++ Modules/Gui/home.nix
@@ -2,6 +2,7 @@
   home.username = "allthebeans";
   home.homeDirectory = "/home/allthebeans";
   home.stateVersion = "24.05";
+  programs.bash.enable = true;
   home = {
     sessionPath = [ "$HOME/.emacs.d/bin" ];
     sessionVariables = {
