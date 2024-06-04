a2ae8271a289df1dd80e19d34f929ab955a25f67
Nathan-R-Wood <nathanroywood@proton.me>
2024-06-04 10:50:27 -0600

    testing doom

--- Modules/Gui/home.nix
+++ Modules/Gui/home.nix
@@ -13,7 +13,7 @@
     enable = true;
     configFile = {
       "$HOME/.emacs.d" = {
-        source = pkgs.fetchgit { url = "https://github.com/doomemacs/doomemacs"; hash = ""; };
+        source = builtins.fetchGit { url = "https://github.com/doomemacs/doomemacs"; rev = "517daa4ed9168855c202ba2fd28920f6ee17249f"; };
       };
     };
   };
