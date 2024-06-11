87b27728d8cc9c1530272052b4b00b3a47c32e19
nathan-r-wood <nathanroywood@proton.me>
2024-06-11 01:38:33 -0600

    sddm launches in wayland now for real

--- Systems/Gaming-Desktop/configuration.nix
+++ Systems/Gaming-Desktop/configuration.nix
@@ -24,6 +24,7 @@ imports =
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.enable = true; # Naming in nix is still cursed. Need this for sddm even wayland only.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   # Hint to Electron that it's running under wayland.
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
--- Systems/Laptop/configuration.nix
+++ Systems/Laptop/configuration.nix
@@ -27,6 +27,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.enable = true; # Naming in nix is still cursed. Need this for sddm even wayland only.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Enable CUPS to print documents.
