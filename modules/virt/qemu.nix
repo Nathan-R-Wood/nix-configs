ccbb834698c33764621a2093985ddfe1052d966b
nathan-r-wood <nathanroywood@proton.me>
2024-06-22 17:36:57 -0600

    binfmt testing

--- modules/virt/qemu.nix
+++ modules/virt/qemu.nix
@@ -2,4 +2,10 @@
    environment.systemPackages = with pkgs; [
       qemu
    ];
+
+   # For podman multiarch builds
+   boot.binfmt.emulatedSystems = [
+      "aarch64-linux"
+      "aarch64_be-linux"
+   ];
 }
