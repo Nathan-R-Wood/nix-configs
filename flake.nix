528649c2d068f62b9de7cf276d98790fd8c799c7
nathan-r-wood <nathanroywood@proton.me>
2024-06-26 14:04:23 -0600

    updated home-manager

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-24.05";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-        home-manager.url = "github:nix-community/home-manager/a631666";
+        home-manager.url = "github:nix-community/home-manager/release-24.05";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
