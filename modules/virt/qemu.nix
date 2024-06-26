8512289ecb2de085f90bdd4162d3a08b9c83b9ef
nathan-r-wood <nathanroywood@proton.me>
2024-06-26 15:14:09 -0600

    appimages maybe

--- modules/virt/qemu.nix
+++ modules/virt/qemu.nix
@@ -6,6 +6,14 @@
    # For podman multiarch builds
    boot.binfmt.emulatedSystems = [
       "aarch64-linux"
-      "aarch64_be-linux"
    ];
+
+   boot.binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+   };
 }
