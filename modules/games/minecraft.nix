8666e384317380c345d00fc644f5f30dcc1873ef
nathan-r-wood <nathanroywood@proton.me>
2024-06-30 10:28:27 -0600

    flake update

--- modules/games/minecraft.nix
+++ modules/games/minecraft.nix
@@ -3,6 +3,4 @@
     environment.systemPackages = with pkgs; [
         prismlauncher
     ];
-    # this doesn't exist so no easy to set up lan because why would syntax in this god forsaken operating system stay consistent
-    # programs.prismlauncher.enable = true;
 }
