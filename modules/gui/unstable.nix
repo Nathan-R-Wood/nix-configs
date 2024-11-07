f0e09a5501f9be90eda363b93f3c6e184845ed22
nathan-r-wood <nathanroywood@proton.me>
2024-11-07 13:04:02 -0700

    blender is brokey for now

--- modules/gui/unstable.nix
+++ modules/gui/unstable.nix
@@ -3,6 +3,6 @@
     environment.systemPackages = with pkgs-unstable; [
         prusa-slicer
         # this package of blender is not cuda compatible need to enable cuda support and use "blender-bin" for optix denoising to work but thats a problem for later.
-        blender
+        # blender
     ];
 }
