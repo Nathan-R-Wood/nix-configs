b897fde6f3a4980bcc8bf657ef91efd11b2d7985
nathan-r-wood <nathanroywood@proton.me>
2025-03-08 00:12:18 -0700

    default pass and flake update

--- modules/users/allthebeans.nix
+++ modules/users/allthebeans.nix
@@ -3,6 +3,7 @@
   users.users.allthebeans = {
     shell = pkgs.zsh;
     isNormalUser = true;
+    initialHashedPassword = "$y$j9T$UGz9QqII.qZSA.zMm/gUe.$FYkgAKvaqS74PjnO5XLQQk6VgwH5Ky3UmMorYMNHjb2";
     description = "Some name or something";
     extraGroups = [ "networkmanager" "wheel" "dialout" "adbusers" ];
     openssh.authorizedKeys.keys = [
