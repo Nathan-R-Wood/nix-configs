3fa3b39254bc6b7fc14191cb112686e9538dad61
Nathan-R-Wood <nathanroywood@proton.me>
2025-05-26 17:41:22 -0600

    still testing weird gpu iconsistencies

--- modules/gui/de/cosmic.nix
+++ modules/gui/de/cosmic.nix
@@ -4,6 +4,7 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
   services.xserver.enable = true;
+  programs.xwayland.enable = true;
   # Hint to Electron that it's running under wayland.
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   users.groups.video.members = [ "allthebeans" ]; #honestly out of other ideas for weird video issues
