389e7296ac2a6a284cc9140009dea2bb26b38bfb
nathan-r-wood <nathanroywood@proton.me>
2025-08-04 20:28:45 -0600

    added user to nixbld group

--- modules/users/allthebeans.nix
+++ modules/users/allthebeans.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     initialHashedPassword = "$y$j9T$UGz9QqII.qZSA.zMm/gUe.$FYkgAKvaqS74PjnO5XLQQk6VgwH5Ky3UmMorYMNHjb2";
     description = "Some name or something";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "nixbld" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg26eeOJ8a2idJmHGH1/3aApR8nf/kw2QYbNpQ7cjcn wizarddragonpublic@gmail.com"
     ];
