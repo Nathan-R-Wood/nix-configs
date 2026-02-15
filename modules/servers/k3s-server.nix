bf0937f7fa77cd6f02bdfbcdadee4fd76c23222a
nathan-r-wood <nathanroywood@proton.me>
2026-02-14 23:43:06 -0700

    k3s

--- modules/servers/k3s-server.nix
+++ modules/servers/k3s-server.nix
@@ -4,7 +4,6 @@
     enable = true;
     role = "server";
     tokenFile = "/home/allthebeans/.k3s-token";
-    serverAddr = "https://jar-bairn.tailcbbdd7.ts.net:6443";
     clusterInit = true;
   };
 
