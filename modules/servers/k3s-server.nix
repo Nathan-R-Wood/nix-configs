2b65539fb55c4800057eb2407e6ef67cc43cf6ed
nathan-r-wood <nathanroywood@proton.me>
2026-02-15 00:13:28 -0700

    k3s reset

--- modules/servers/k3s-server.nix
+++ modules/servers/k3s-server.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }: {
 
   services.k3s = {
-    enable = true;
+    enable = false;
     role = "server";
     tokenFile = "/home/allthebeans/.k3s-token";
     clusterInit = true;
   };
+   services.etcd.enable = false;
 
 }
