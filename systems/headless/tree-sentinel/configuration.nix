b0dc2a602ce560f0e43b1a9726ee2346a6b53b9a
nathan-r-wood <nathanroywood@proton.me>
2026-02-27 15:12:00 -0700

    tree-sentinel watchdog fix

--- systems/headless/tree-sentinel/configuration.nix
+++ systems/headless/tree-sentinel/configuration.nix
@@ -7,7 +7,16 @@
     ];
   networking.hostName = "Tree-sentinel"; # Define your hostname.
   networking.networkmanager.enable = true;
-  networking.firewall.enable = false; 
+  networking.firewall.enable = false;
+
+  services.watchdogd = {
+    enable = true;
+    settings = {
+      interval = lib.mkForce 5;
+      timeout = lib.mkForce 120;
+      safe-exit = lib.mkForce false;
+    };
+  };
 
   boot.loader.grub.enable = lib.mkForce true;
   boot.loader.grub.device = "/dev/sda";
