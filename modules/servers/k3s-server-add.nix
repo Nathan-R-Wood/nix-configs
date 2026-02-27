d3e277ad1bbef9b3d72909a76e70980c7460706e
nathan-r-wood <nathanroywood@proton.me>
2026-02-27 14:49:47 -0700

    k3s multi server tests

--- modules/servers/k3s-server-add.nix
+++ modules/servers/k3s-server-add.nix
@@ -3,6 +3,7 @@
   services.k3s = {
     enable = true;
     role = "server";
+    clusterInit = true;
     tokenFile = "/home/allthebeans/.k3s-token";
     serverAddr = "https://jar-bairn.tailcbbdd7.ts.net:6443";
   };
