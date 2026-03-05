c86087688eceb69e01f01a5b1b40e301665e20a4
nathan-r-wood <nathanroywood@proton.me>
2026-03-05 02:39:12 -0700

    k3s reset again

--- modules/servers/k3s-server-add.nix
+++ modules/servers/k3s-server-add.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   
   services.k3s = {
-    enable = false;
+    enable = true;
     role = "server";
     tokenFile = "/home/allthebeans/.k3s-token";
     serverAddr = "https://jar-bairn.tailcbbdd7.ts.net:6443";
