c33eb5939bfb36eb3af502104079af35745d1621
nathan-r-wood <nathanroywood@proton.me>
2026-03-04 19:59:54 -0700

    firmware partition not mounted on birthday-boy-2

--- systems/headless/birthday-boy-2/birthday-boy-2.nix
+++ systems/headless/birthday-boy-2/birthday-boy-2.nix
@@ -15,6 +15,10 @@
     { device = lib.mkForce "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
     };
+  fileSystems."/boot/firmware" =
+    { device = lib.mkForce "/dev/disk/by-label/FIRMWARE";
+      fsType = "ext4";
+    };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
