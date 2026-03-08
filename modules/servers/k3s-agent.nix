0899564f8bf40f082d7d7aaa937169a91228a5ec
nathan-r-wood <nathanroywood@proton.me>
2026-03-08 03:29:30 -0600

    k3s vpn doesn't need to be specified on agent

--- modules/servers/k3s-agent.nix
+++ modules/servers/k3s-agent.nix
@@ -6,7 +6,6 @@
     tokenFile = "/home/allthebeans/.k3s-token";
     serverAddr = "https://jar-bairn.tailcbbdd7.ts.net:6443";
     extraFlags = [
-      "--flannel-backend=wireguard-native"
     ];
   };
   
