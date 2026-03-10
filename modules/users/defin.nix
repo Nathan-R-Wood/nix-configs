51ddb388844a7bf1faca937df48ab8cf54d08679
nathan-r-wood <nathanroywood@proton.me>
2026-03-10 03:21:32 -0600

    devfin keys

--- modules/users/defin.nix
+++ modules/users/defin.nix
@@ -7,6 +7,7 @@ users.users.defin = {
     packages = with pkgs; [helix];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrDUymBW+rosSI68sxDKLMfH7cOPPLG5K+CuA6aYcsp defin@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDatxD3mSPG8/72Ka0uuGpGHrwh6N718ZCLCOkOLwmM defin@doretta"
     ];
   };
 nix.settings.allowed-users = ["@builders" "@wheel"];
